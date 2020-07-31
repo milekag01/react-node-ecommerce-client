3787f12ec223f91552b59766a39d10b824649a26
milek agrawal <milekag01@gmail.com>
2020-07-31 08:50:34 +0530

    change project structure and add order, admin and product components

---
+++ src/core/fixedPrices.js
@@ -0,0 +1,32 @@
+export const prices = [
+    {
+        _id: 0,
+        name: "Any",
+        array: []
+    },
+    {
+        _id: 1,
+        name: "Rs.0 to Rs.20000",
+        array: [0, 20000]
+    },
+    {
+        _id: 2,
+        name: "Rs.20001 to Rs.40000",
+        array: [20001, 40000]
+    },
+    {
+        _id: 3,
+        name: "Rs.40001 to Rs.60000",
+        array: [40001, 60000]
+    },
+    {
+        _id: 4,
+        name: "Rs.60001 to Rs.80000",
+        array: [60001, 80000]
+    },
+    {
+        _id: 5,
+        name: "More than Rs.80000",
+        array: [80001, 300000]
+    }
+];
